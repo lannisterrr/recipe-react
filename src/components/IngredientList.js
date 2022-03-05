3e4b441a1da760cbe5d06d53934fa57c102c8e1f
siddhant <siddhantp736@gmail.com>
2022-03-05 16:22:02 +0530

    WIP : Author jsx and styling done

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -5,5 +5,14 @@ export default function IngredientList({ ingredients }) {
   const ingredientElements = ingredients.map(ingredient => {
     return <Ingredient key={ingredient.id} {...ingredient} />;
   });
-  return <div className="ingredient-grid f-6 m-v-4 ">{ingredientElements}</div>;
+  return (
+    <>
+      <div className="ingredient-grid f-6 m-v-4 ">{ingredientElements}</div>
+      <div className="author-text-container">
+        <p className="heading-4 t-c-3 author-text p-4">
+          <span className="t-c-1">Author : </span> &nbsp; Siddhant
+        </p>
+      </div>
+    </>
+  );
 }
